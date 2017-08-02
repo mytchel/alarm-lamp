d3f47be0ba33d19116e1211b2f5223be92ac8006
Mytchel Hammond <mytchel@openmailbox.org>
2017-08-02 20:34:16 +1200

    Different way of doing it. Turn on at ovf, off at compa.
    This is how it is done in hardware with PWM which is the next
    thing to try.

--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 
 #define LAMP    (1 << 0)
 
-volatile uint8_t lamp_brightness = 20;
-volatile uint32_t t = 0;
-
 int
 main(void)
 {
@@ -14,12 +11,17 @@ main(void)
 	
 	TCCR1 |= (1<<CS13)|(1<<CS12)|(1<<CS11)|(1<<CS10);
 	TCNT1 = 0;
+	OCR1A = 200;
 	
 	while (1) {
 		if (TIFR & (1<<TOV1)) {
 			PORTB |= LAMP;
-		} else {
+			TIFR = (1<<TOV1);
+		}
+		
+		if (TIFR & (1<<OCF1A)) {
 			PORTB &= ~LAMP;
+			TIFR = (1<<OCF1A);
 		}
 	}
 }
